03684ac5981469543b2e3b846ac44d1980bba980
Yerkebulan <kundyzbekov98@gmail.com>
2019-09-18 21:18:43 +0600

    Initial Commit

---
+++ Calculator_mvc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator_mvc
+//
+//  Created by Еркебулан Кундызбеков on 9/18/19.
+//  Copyright © 2019 Еркебулан Кундызбеков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
